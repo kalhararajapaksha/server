8ca36f91b112f51e5ddfc710d7073642f432e08c
kalhararajapaksha <rmkkrajapaksha@students.nsbm.ac.lk>
2023-02-01 01:56:09 +0530

    change add

--- src/user.routes.ts
+++ src/user.routes.ts
@@ -23,7 +23,7 @@ userRouter.get("/", async (_req, res) => {
         res.status(500).send(error.message);
     }
 });
-
+//
 userRouter.post("/loginn", async (req, res) => {
     try {
         // const { username, password } = req.query;
